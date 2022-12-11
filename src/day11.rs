a57955ccfe9ec74549937dc4057e89890d9c54eb
BatteredBunny <ayes2022@protonmail.com>
2022-12-11 21:56:16 +0200

    day 11 fancy

--- src/day11.rs
+++ src/day11.rs
@@ -13,54 +13,46 @@ fn sign_to_func(s: &str) -> fn(i64, i64) -> Option<i64> {
     match s {
         "*" => i64::checked_mul,
         "+" => i64::checked_add,
-        "-" => i64::checked_sub,
-        "/" => i64::checked_div,
         _ => unreachable!(),
     }
 }
 
 impl Monkey {
-    fn from_lines(lines: [&str; 6]) -> Monkey {
+    fn from_lines(lines: [&str; 6]) -> Option<Monkey> {
         let items: Vec<i64> = lines[1]
-            .trim_start_matches("  Starting items: ")
+            .strip_prefix("  Starting items: ")?
             .split(", ")
             .map(|num| num.parse().unwrap())
             .collect();
 
         let operation_arguments: Vec<&str> = lines[2]
-            .trim_start_matches("  Operation: new = ")
+            .strip_prefix("  Operation: new = ")?
             .split_whitespace()
             .collect();
 
         let sign = sign_to_func(operation_arguments[1]);
         let operation: Box<dyn Fn(i64) -> i64> =
             if operation_arguments[0] == "old" && operation_arguments[2] == "old" {
-                Box::new(move |x: i64| {
-                    // println!("{x}");
-                    sign(x, x).unwrap()
-                })
+                Box::new(move |x: i64| sign(x, x).unwrap())
             } else {
-                let arg = operation_arguments[2].parse().unwrap();
-                Box::new(move |x: i64| {
-                    // println!("{x} {arg}");
-                    sign(x, arg).unwrap()
-                })
+                let arg = operation_arguments[2].parse().ok()?;
+                Box::new(move |x: i64| sign(x, arg).unwrap())
             };
 
         let divisible_by = lines[3]
-            .trim_start_matches("  Test: divisible by ")
-            .parse::<i64>()
-            .unwrap();
+            .strip_prefix("  Test: divisible by ")?
+            .parse()
+            .ok()?;
 
         let true_monkey = lines[4]
-            .trim_start_matches("    If true: throw to monkey ")
-            .parse::<u16>()
-            .unwrap();
+            .strip_prefix("    If true: throw to monkey ")?
+            .parse()
+            .ok()?;
 
         let false_monkey = lines[5]
-            .trim_start_matches("    If false: throw to monkey ")
-            .parse::<u16>()
-            .unwrap();
+            .strip_prefix("    If false: throw to monkey ")?
+            .parse()
+            .ok()?;
 
         let test: Box<dyn Fn(i64) -> u16> = Box::new(move |x: i64| {
             if x % divisible_by == 0 {
@@ -70,13 +62,13 @@ impl Monkey {
             }
         });
 
-        Monkey {
+        Some(Monkey {
             inspected_items: 0,
             items,
             operation,
             divisible_by,
             test,
-        }
+        })
     }
 }
 
@@ -115,7 +107,7 @@ pub fn part1(input: &str) -> i64 {
             it.next();
             lines
         })
-        .map(Monkey::from_lines)
+        .map(|lines| Monkey::from_lines(lines).unwrap())
         .collect();
 
     logic(&mut monkeys, 20, Box::new(|x| x / 3))
@@ -130,7 +122,7 @@ pub fn part2(input: &str) -> i64 {
             it.next();
             lines
         })
-        .map(Monkey::from_lines)
+        .map(|lines| Monkey::from_lines(lines).unwrap())
         .collect();
 
     let divider: i64 = monkeys.iter().map(|monkey| monkey.divisible_by).product();
