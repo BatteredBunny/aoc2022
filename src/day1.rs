191c61ce0daa25cc8568ebd1cd5b1d14500b3017
BatteredBunny <ayes2022@protonmail.com>
2022-12-11 16:45:12 +0200

    day 1 change

--- src/day1.rs
+++ src/day1.rs
@@ -6,35 +6,30 @@ fn calculate_elf_calories(elf: &str) -> u64 {
         .sum()
 }
 
+#[aoc_generator(day1)]
+fn input_generator(input: &str) -> Vec<u64> {
+    input.split("\n\n").map(calculate_elf_calories).collect()
+}
+
 #[aoc(day1, part1)]
-pub fn part1(input: &str) -> u64 {
-    input
-        .split("\n\n")
-        .map(calculate_elf_calories)
-        .max()
-        .unwrap()
+pub fn part1(calories: &[u64]) -> u64 {
+    *calories.iter().max().unwrap()
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &str) -> u64 {
-    input
-        .split("\n\n")
-        .map(calculate_elf_calories)
-        .sorted()
-        .rev()
-        .take(3)
-        .sum()
+pub fn part2(calories: &[u64]) -> u64 {
+    calories.iter().sorted().rev().take(3).sum()
 }
 
 #[cfg(test)]
 mod tests {
     use std::fs::read_to_string;
 
-    use crate::day1::{part1, part2};
+    use crate::day1::{input_generator, part1, part2};
 
     #[test]
     fn test_day1() {
-        let input = read_to_string("input/2022/day1.txt").unwrap();
+        let input = input_generator(&read_to_string("input/2022/day1.txt").unwrap());
 
         assert_eq!(67658, part1(&input));
         assert_eq!(200158, part2(&input));
